1252ed268c49f0a2d190e62b05ecbaffee50e62f
t-rex600 <fisch890@gmail.com>
2016-07-14 11:28:54 +0200

    mako: Update fingerprint to MOB30R

--- liquid.mk
+++ liquid.mk
@@ -22,8 +22,8 @@ PRODUCT_MANUFACTURER := LGE
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
    PRODUCT_NAME=occam \
-   BUILD_FINGERPRINT=google/occam/mako:6.0.1/MTC19T/2741993:user/release-keys \
-   PRIVATE_BUILD_DESC="occam-user 6.0.1 MTC19T 2741993 release-keys"
+   BUILD_FINGERPRINT=google/occam/mako:6.0.1/MOB30R/2775141:user/release-keys \
+   PRIVATE_BUILD_DESC="occam-user 6.0.1 MOB30R 2775141 release-keys"
    
 # Enable Torch
 PRODUCT_PACKAGES += Torch   
